f1a568d8a532642da9b775ad1057aeda360595af
Jeremi Stadler <stadler.jeremi@gmail.com>
2015-07-26 21:44:03 +0200

    Removed rasterizer testing code

--- scripts/app.ts
+++ scripts/app.ts
@@ -12,7 +12,7 @@ if (debug) {
 var loadDna = function (onComplete: (dna: Dna) => void) {
     if (debug) {
         window.setTimeout(function () {
-            var dna = localStorage.getItem('dna');
+            var dna = localStorage.getItem('dna2');
             if (!dna)
                 onComplete(DnaEvolver.CreateDna(10, 'me.jpg'));
             else
@@ -73,61 +73,4 @@ var loadedAll = function (dna, image) {
     //var game = new WebGLRasterizer(webgl, image, dna);
 
     var rasterizer = new JsRasterizer(image, dna);
-
-    var buffer = new Uint8Array(10 * 10 * 4);
-    for (var i = 0; i < buffer.length; i++)
-        buffer[i] = 255;
-
-    //Raster.drawPolygon(buffer, 10, [-100, -100,  -10, 20,  20, 20], [255, 0, 0, 0.3]);
-    Raster.drawPolygon(buffer, 10, [0, 0, 5, 5, 0, 10], [255, 0, 0, 0.3]);
-    //Raster.drawPolygon(buffer, 10, [5, -10, -10, 20, 20, 20], [0, 255, 0, 0.3]);
-    //Raster.drawPolygon(buffer, 10, [-1, 1, 1, 9, 9, 9], [0, 128, 0, 0.1]);
-    //Raster.drawPolygon(buffer, 10, [10, 0,  10, 10,  0, 10], [0, 100, 0, 1]);
-
-    var canvas = document.createElement('canvas');
-    canvas.width = 10;
-    canvas.height = 10;
-    var ctx = canvas.getContext('2d');
-    ctx.fillStyle = 'white';
-    ctx.fillRect(0, 0, 10, 10);
-    var data = ctx.createImageData(10, 10);
-
-    for (var i = 0; i < data.data.length; i++) {
-        data.data[i] = buffer[i];
-    }
-
-    ctx.putImageData(data, 0, 0);
-    document.body.appendChild(canvas);
-    canvas.style.width = '200px';
-    canvas.style.height = '200px';
-    canvas.style.imageRendering = 'pixelated';
-
-
-
-    var canvas = document.createElement('canvas');
-    canvas.width = 10;
-    canvas.height = 10;
-    var ctx = canvas.getContext('2d');
-    ctx.fillStyle = 'white';
-    ctx.fillRect(0, 0, 10, 10);
-    document.body.appendChild(canvas);
-    canvas.style.width = '200px';
-    canvas.style.height = '200px';
-    canvas.style.imageRendering = 'pixelated';
-
-    ctx.fillStyle = 'rgba(255, 0, 0, 0.3)';
-    ctx.beginPath();
-    ctx.moveTo(0, 0);
-    ctx.lineTo(5, 5);
-    ctx.lineTo(0, 10);
-    ctx.closePath();
-    ctx.fill();
-
-    //ctx.fillStyle = 'rgba(0, 100, 0, 0.3)';
-    //ctx.beginPath();
-    //ctx.moveTo(1, 1);
-    //ctx.lineTo(10, 5);
-    //ctx.lineTo(3, 6);
-    //ctx.closePath();
-    //ctx.fill();
 }
