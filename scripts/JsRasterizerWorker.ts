1ca328764381c87f70800e4e0640a0ade6d1757d
Jeremi Stadler <stadler.jeremi@gmail.com>
2015-07-26 22:47:19 +0200

    Removed old WebGL stuff like Texture and DnaEvolver

--- scripts/JsRasterizerWorker.ts
+++ scripts/JsRasterizerWorker.ts
@@ -1,14 +1,13 @@
-﻿var globalWidth = 64;
-var globalHeight = 64;
+﻿var globalWidth = 256;
+var globalHeight = 256;
 
 class JsRasterizerWorker {
     tempBuffer: Uint8Array;
 
-
     constructor(public sourceImageData: number[]) {
         this.tempBuffer = new Uint8Array(globalWidth * globalHeight * 4);
     }
-
+    
     draw(dna: Dna) {
         var startTime = new Date().getTime();
 
@@ -25,9 +24,15 @@ class JsRasterizerWorker {
         console.log('Generation time: ', (new Date().getTime() - startTime) / iterations, 'ms', 'fittness', dna.Fitness, 'Mutations: ', dna.Mutation, 'Generations: ', dna.Generation, 'Genes: ', dna.Genes.length);
 
         var sum = GeneMutator.GeneMutators.map(f => f.effectiveness).reduce((a, b) => a + b);
-        console.log('Mutators: ', GeneMutator.GeneMutators.map(f => f.name + ': ' + Math.round(f.effectiveness / sum * 100)).join('%, ') + '%');
+        console.log('Mutators: ', GeneMutator.GeneMutators.map(f => f.name + ': ' + Math.round(f.effectiveness / sum * 1000)).join('%, ') + '%');
 
-        self.postMessage(dna);
+        var mut = GeneMutator.GeneMutators.map(f => {
+            return { eff: f.effectiveness, name: f.name }
+        });
+        mut.sort((a, b) => a.name.localeCompare(b.name));
+
+        self.postMessage({ dna: dna, mutations: mut }, null);
+
     }
 
     doIteration(mutator, dna: Dna) {
@@ -69,7 +74,7 @@ class JsRasterizerWorker {
         var diff = 0.0;
         for (var i = 0; i < buff1.length; i++) {
             var q = Math.abs(buff1[i] - buff2[i]);
-            diff += q;
+            diff += q * q;
         }
         return diff;
     }
@@ -95,8 +100,8 @@ interface IGeneMutator {
 
 class GeneMutator {
     static StartingEffectiveness = 1000000;
-    static EffectivenessChangeRate = 0.2;
-    static MinimumEffectiveness = 0.001;
+    static EffectivenessChangeRate = 0.005;
+    static MinimumEffectiveness = 0.00001;
 
     public static DefaultMutateGene(dna: Dna) {
         var gene = new Gene();
@@ -234,70 +239,6 @@ class Utils {
         tickLoop(0);
     }
 
-    static createProgram(gl: WebGLRenderingContext, name: string): WebGLProgram {
-        var p = gl.createProgram();
-        gl.attachShader(p, Utils.getShader(gl, name + '-vs'));
-        gl.attachShader(p, Utils.getShader(gl, name + '-fs'));
-        gl.linkProgram(p);
-
-        if (!gl.getProgramParameter(p, gl.LINK_STATUS))
-            console.error("Unable to initialize the shader program.");
-
-        return p;
-    }
-
-
-    static getShader(gl: WebGLRenderingContext, id: string) {
-        var shaderScript = <HTMLScriptElement>document.getElementById(id);
-
-        if (!shaderScript)
-            console.error('Shader not found', id);
-
-        var shader = null;
-
-        if (shaderScript.type == "x-shader/x-fragment") {
-            shader = gl.createShader(gl.FRAGMENT_SHADER);
-        } else if (shaderScript.type == "x-shader/x-vertex") {
-            shader = gl.createShader(gl.VERTEX_SHADER);
-        } else {
-            console.error('Bad shader type', id, shaderScript.type);
-        }
-
-        gl.shaderSource(shader, shaderScript.innerHTML);
-        gl.compileShader(shader);
-
-        if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS))
-            console.error("Shader compile failed", gl.getShaderInfoLog(shader));
-
-        return shader;
-    }
-
-    static createFramebuffer(gl: WebGLRenderingContext) {
-        var tex = gl.createTexture();
-        gl.bindTexture(gl.TEXTURE_2D, tex);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, globalWidth, globalHeight, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
-
-        var buf = gl.createFramebuffer();
-        gl.bindFramebuffer(gl.FRAMEBUFFER, buf);
-        gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, tex, 0);
-
-        return new FramebufferWrapper(tex, buf);
-    }
-
-    static setRectangle(gl: WebGLRenderingContext, x: number, y: number, width: number, height: number) {
-        var x2 = x + width;
-        var y1 = y + height;
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-            x, y1, x2, y1, x, y, x, y, x2, y1, x2, y]), gl.STATIC_DRAW);
-    }
-
-    static setRectangleTex(gl: WebGLRenderingContext) {
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-            0.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.0]), gl.STATIC_DRAW);
-    }
-
     static randomIndex(arr: any[]) {
         return Math.floor(Math.random() * arr.length);
     }
@@ -339,206 +280,6 @@ class Dna {
     Mutation: number;
     Fitness: number;
     Organism: Organism;
-}
-
-
-class DnaEvolver {
-    static PositionsPerGene: number = 3;
-
-    EvolvingGene: Gene;
-    EvolvingGeneIndex: number;
-
-    ColorBuffer: WebGLBuffer;
-    PosBuffer: WebGLBuffer;
-
-    Risk: number;
-    LastSaved: number;
-
-    constructor(public webgl: WebGLRenderingContext, public Dna: Dna, public program: WebGLProgram) {
-        this.PosBuffer = webgl.createBuffer();
-        this.ColorBuffer = webgl.createBuffer();
-        this.Risk = 0;
-
-
-        var posArr = new Float32Array(this.Dna.Genes.length * DnaEvolver.PositionsPerGene * 2);
-        var colorArr = new Float32Array(this.Dna.Genes.length * DnaEvolver.PositionsPerGene * 4);
-
-        webgl.bindBuffer(webgl.ARRAY_BUFFER, this.PosBuffer);
-        webgl.bufferData(webgl.ARRAY_BUFFER, posArr, webgl.DYNAMIC_DRAW);
-
-        webgl.bindBuffer(webgl.ARRAY_BUFFER, this.ColorBuffer);
-        webgl.bufferData(webgl.ARRAY_BUFFER, colorArr, webgl.DYNAMIC_DRAW);
-
-        for (var i = 0; i < this.Dna.Genes.length; i++) {
-            this.Dna.Genes[i].Color.length = 4;
-            this.SetTriangleToBuffers(this.Dna.Genes[i], i);
-        }
-
-        this.LastSaved = new Date().getTime();
-    }
-
-    static CreateDna(numberOfGenes: number, image: string) {
-        var dna = new Dna();
-        dna.Fitness = Infinity;
-        dna.Genes = new Array(numberOfGenes);
-        dna.Generation = 0;
-        dna.Mutation = 0;
-        dna.Organism = new Organism();
-        dna.Organism.ImagePath = image;
-        dna.Organism.GeneCount = numberOfGenes;
-
-        for (var i = 0; i < numberOfGenes; i++) {
-            var gene = dna.Genes[i] = new Gene();
-            gene.Color = [Math.random(), Math.random(), Math.random(), Math.random() * 0.8 + 0.2];
-            gene.Pos = new Array(DnaEvolver.PositionsPerGene * 2);
-            for (var q = 0; q < gene.Pos.length; q++)
-                gene.Pos[q] = 0;
-        }
-
-        return dna;
-    }
-
-    findBestGenePos(geneIndex: number, orginalImage: Int8Array) {
-        var bestMatch = null;
-        var fitness = Infinity;
-
-        var triPixels = new Uint8Array(globalWidth * globalHeight * 4);
-        for (var iteration = 0; iteration < 100; iteration++) {
-            var tri = new Gene();
-
-            tri.Color = [Math.random(), Math.random(), Math.random(), Math.random() * 0.4 + 0.2];
-            tri.Pos = new Array(DnaEvolver.PositionsPerGene * 2);
-            for (var i = 0; i < tri.Pos.length; i++)
-                tri.Pos[i] = Math.random() * 1.2 - 0.1;
-
-            this.SetTriangleToBuffers(tri, geneIndex);
-
-            this.Draw();
-
-            this.webgl.readPixels(0, 0, globalWidth, globalHeight, this.webgl.RGBA, this.webgl.UNSIGNED_BYTE, triPixels);
-            var newFitness = this.calculateFitness(orginalImage, triPixels);
-
-            if (newFitness < fitness) {
-                bestMatch = tri;
-                fitness = newFitness;
-            }
-        }
-
-        this.Dna.Fitness = fitness;
-        this.Dna.Mutation++;
-        this.Dna.Generation++;
-        this.Dna.Genes[geneIndex] = bestMatch;
-        this.SetTriangleToBuffers(bestMatch, geneIndex);
-    }
-
-    calculateFitness(buff1: Int8Array, buff2: Int8Array) {
-        var diff = 0.0;
-
-        for (var i = 0; i < buff1.byteLength; i++) {
-            var q = Math.abs(buff1[i] - buff2[i]);
-            diff += q * q;
-        }
-
-        return diff;
-    }
-
-    StartEvolving(): void {
-        var index = Utils.randomIndex(this.Dna.Genes);
-
-        this.EvolvingGene = this.Dna.Genes[index];
-        this.EvolvingGeneIndex = index;
-
-        var gene = new Gene();
-        this.Dna.Genes[index] = gene;
-
-        if (Math.random() > 0.5) {
-            gene.Color = [Math.random(), Math.random(), Math.random(), 0.2];
-            gene.Pos = new Array(DnaEvolver.PositionsPerGene * 2);
-            for (var i = 0; i < gene.Pos.length; i++)
-                gene.Pos[i] = Math.random() * 1.2 - 0.1;
-        }
-        else if (Math.random() > 0.3) {
-            var oldGene = this.EvolvingGene;
-            gene.Color = oldGene.Color.slice();
-            gene.Pos = oldGene.Pos.slice();
-
-            var indexToMove = Utils.randomIndex(gene.Pos);
-            gene.Pos[indexToMove] += (Math.random() - 0.5) * 0.1;
-        }
-        else {
-            var oldGene = this.EvolvingGene;
-            gene.Color = oldGene.Color.slice();
-            gene.Pos = oldGene.Pos.slice();
-
-            var indexToChange = Utils.randomFromTo(0, 3);
-            gene.Color[indexToChange] = Utils.ClampFloat((Math.random() - 0.5) * 0.1 + gene.Color[indexToChange]);
-        }
-
-        this.SetTriangleToBuffers(gene, index);
-    }
-
-    EndEvolving(fitness: number): void {
-        if (fitness < this.Dna.Fitness + this.Risk * 100) {
-            this.Dna.Fitness = fitness;
-            this.Dna.Mutation++;
-            //console.log('fitness', fitness, 'generation: ' + this.Dna.Generation, 'Risk', this.Risk, 'index', this.EvolvingGeneIndex);
-
-            this.Risk = 0;
-
-            var dateNow = new Date().getTime();
-            var saveTime = this.Dna.Mutation > 1000 ? 5 : 20
-            if (dateNow > this.LastSaved + 1000 * saveTime) {
-                this.Save();
-                this.LastSaved = dateNow;
-            }
-        }
-        else {
-            this.Dna.Genes[this.EvolvingGeneIndex] = this.EvolvingGene;
-            this.SetTriangleToBuffers(this.EvolvingGene, this.EvolvingGeneIndex);
-            this.Risk++;
-        }
-
-        this.Dna.Generation++;
-    }
-
-    Save() {
-        var xhr = new XMLHttpRequest();
-        xhr.open('POST', baseUrl + '/api/dna/save', true);
-        xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-        xhr.send(JSON.stringify(this.Dna));
-    }
-
-    SetTriangleToBuffers(tri: Gene, index: number) {
-        var posBuff = new Float32Array(tri.Pos);
-        var colorBuff = new Float32Array(4 * DnaEvolver.PositionsPerGene);
-
-        for (var p = 0; p < DnaEvolver.PositionsPerGene; p++)
-            for (var i = 0; i < 4; i++)
-                colorBuff[p * 4 + i] = tri.Color[i];
-
-        this.webgl.bindBuffer(this.webgl.ARRAY_BUFFER, this.PosBuffer);
-        this.webgl.bufferSubData(this.webgl.ARRAY_BUFFER, index * posBuff.byteLength, posBuff);
-
-        this.webgl.bindBuffer(this.webgl.ARRAY_BUFFER, this.ColorBuffer);
-        this.webgl.bufferSubData(this.webgl.ARRAY_BUFFER, index * colorBuff.byteLength, colorBuff);
-    }
-
-    Draw() {
-        var gl = this.webgl;
-
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.ColorBuffer);
-        var colorLocation = gl.getAttribLocation(this.program, "a_color");
-        gl.enableVertexAttribArray(colorLocation);
-        gl.vertexAttribPointer(colorLocation, 4, gl.FLOAT, false, 0, 0);
-
-
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.PosBuffer);
-        var positionLocation = gl.getAttribLocation(this.program, "a_position");
-        gl.enableVertexAttribArray(positionLocation);
-        gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
-
-        gl.drawArrays(gl.TRIANGLES, 0, this.Dna.Genes.length * 3);
-    }
 }
 
 
--- scripts/Utils.ts
+++ scripts/Utils.ts
@@ -1,9 +1,5 @@
 ﻿///<reference path="references.ts" />
 
-class FramebufferWrapper {
-    constructor(public tex: WebGLTexture, public framebuffer: WebGLFramebuffer) { }
-}
-
 class Utils {
     static StartTick(tickMethod: (dt: number) => void) {
         var oldTime = 0;
@@ -15,70 +11,6 @@ class Utils {
             window.requestAnimationFrame(tickLoop);
         };
         tickLoop(0);
-    }
-
-    static createProgram(gl: WebGLRenderingContext, name: string): WebGLProgram {
-        var p = gl.createProgram();
-        gl.attachShader(p, Utils.getShader(gl, name + '-vs'));
-        gl.attachShader(p, Utils.getShader(gl, name + '-fs'));
-        gl.linkProgram(p);
-
-        if (!gl.getProgramParameter(p, gl.LINK_STATUS))
-            console.error("Unable to initialize the shader program.");
-
-        return p;
-    }
-
-
-    static getShader(gl: WebGLRenderingContext, id: string) {
-        var shaderScript = <HTMLScriptElement>document.getElementById(id);
-
-        if (!shaderScript)
-            console.error('Shader not found', id);
-
-        var shader = null;
-
-        if (shaderScript.type == "x-shader/x-fragment") {
-            shader = gl.createShader(gl.FRAGMENT_SHADER);
-        } else if (shaderScript.type == "x-shader/x-vertex") {
-            shader = gl.createShader(gl.VERTEX_SHADER);
-        } else {
-            console.error('Bad shader type', id, shaderScript.type);
-        }
-
-        gl.shaderSource(shader, shaderScript.innerHTML);
-        gl.compileShader(shader);
-
-        if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS))
-            console.error("Shader compile failed", gl.getShaderInfoLog(shader));
-
-        return shader;
-    }
-
-    static createFramebuffer(gl: WebGLRenderingContext) {
-        var tex = gl.createTexture();
-        gl.bindTexture(gl.TEXTURE_2D, tex);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, globalWidth, globalHeight, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
-
-        var buf = gl.createFramebuffer();
-        gl.bindFramebuffer(gl.FRAMEBUFFER, buf);
-        gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, tex, 0);
-
-        return new FramebufferWrapper(tex, buf);
-    }
-
-    static setRectangle(gl: WebGLRenderingContext, x: number, y: number, width: number, height: number) {
-        var x2 = x + width;
-        var y1 = y + height;
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-            x, y1, x2, y1, x, y, x, y, x2, y1, x2, y]), gl.STATIC_DRAW);
-    }
-
-    static setRectangleTex(gl: WebGLRenderingContext) {
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-            0.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.0]), gl.STATIC_DRAW);
     }
 
     static randomIndex(arr: any[]) {
