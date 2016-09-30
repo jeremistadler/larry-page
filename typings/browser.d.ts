daa4b3b69c4b81228f9798a82039525ecb98d688
Jeremi Stadler <stadler.jeremi@gmail.com>
2016-09-30 11:41:01 +0200

    remove typings

--- typings/browser.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="browser/ambient/react-dom/index.d.ts" />
-/// <reference path="browser/ambient/react/index.d.ts" />
-/// <reference path="browser/ambient/whatwg-fetch/index.d.ts" />
--- typings/main.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="main/ambient/react-dom/index.d.ts" />
-/// <reference path="main/ambient/react/index.d.ts" />
-/// <reference path="main/ambient/whatwg-fetch/index.d.ts" />
