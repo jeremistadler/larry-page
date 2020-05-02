3568ba5bf2d39e0cdc5f58aa11d6a83d850db038
Jeremi Stadler <jeremi.stadler@storytel.com>
2020-05-02 16:38:04 +0200

    Create .prettierrc.js

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+// Duplicated from react repo: https://github.com/facebook/react/blob/master/.prettierrc.js
+module.exports = {
+  bracketSpacing: false,
+  singleQuote: true,
+  jsxBracketSameLine: true,
+  arrowParens: 'avoid',
+  trailingComma: 'all',
+  semi: false,
+  printWidth: 80,
+  parser: 'typescript',
+}
