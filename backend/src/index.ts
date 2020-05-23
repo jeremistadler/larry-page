618d15ac77c123ace0ec31df945ba64e95ef6b6f
Jeremi Stadler <jeremi.stadler@storytel.com>
2020-05-23 11:20:45 +0200

    Add cors for localhost

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,6 +14,18 @@ addEventListener('fetch', (event: FetchEvent) => {
   const url = new URL(event.request.url)
   console.log(url)
 
+  if (event.request.method === 'OPTIONS') {
+    return new Response(null, {
+      status: 204,
+      headers: {
+        'Access-Control-Allow-Credentials': 'true',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Origin': 'http://localhost:1234',
+        'Access-Control-Allow-Headers': 'Content-Type',
+      },
+    })
+  }
+
   if (url.protocol === 'http:') {
     return event.respondWith(
       Response.redirect(event.request.url.replace('http:', 'https:'), 301),
