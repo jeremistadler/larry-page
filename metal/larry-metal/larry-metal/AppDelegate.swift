72887ad7ecf88c0c6eb62dba4e35c0072b6a21a1
Jeremi Stadler <stadler.jeremi@gmail.com>
2021-08-07 16:49:49 +0200

    Init metal

---
+++ metal/larry-metal/larry-metal/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  larry-metal
+//
+//  Created by Jeremi Stadler on 2021-08-07.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet var window: NSWindow!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
+}
