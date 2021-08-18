4c39e5a988d827aa5d95312aeea4579be70223f0
Jeremi Stadler <stadler.jeremi@gmail.com>
2021-08-18 21:13:38 +0200

    Move normal dist to own file

--- rasterizer-js/src/topK.ts
+++ rasterizer-js/src/topK.ts
@@ -1,16 +1,5 @@
 import {Triangle_Buffer} from './types'
 
-function randomNormalDist(): number {
-  let u = 0,
-    v = 0
-  while (u === 0) u = Math.random() //Converting [0,1) to (0,1)
-  while (v === 0) v = Math.random()
-  let num = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v)
-  num = num / 10.0 + 0.5 // Translate to 0 -> 1
-  if (num > 1 || num < 0) return randomNormalDist() // resample between 0 and 1
-  return num
-}
-
 export async function runPSO(
   cost_func: (data: Triangle_Buffer) => number,
   featureCount: number,
