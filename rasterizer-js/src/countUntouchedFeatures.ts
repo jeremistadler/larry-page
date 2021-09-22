1823f7eaa89c9e6fd5d59aefd079db2c2211f487
Jeremi Stadler <stadler.jeremi@gmail.com>
2021-09-22 09:15:59 +0200

    Update countUntouchedFeatures.ts

--- rasterizer-js/src/countUntouchedFeatures.ts
+++ rasterizer-js/src/countUntouchedFeatures.ts
@@ -7,7 +7,7 @@ export function countUntouchedFeatures(
   let count = 0
 
   for (let i = pos.length - 1; i >= 0; i--) {
-    if (pos[i] === bounds[i][0]) {
+    if (pos[i] === bounds[i][0] || pos[i] === 0) {
       count++
     } else {
       return count
